135dd236aca787fa9437ac6d1ef2973eeb321f7f
anandgmenon <anandgmenon02@gmail.com>
2016-10-09 12:11:22 +0530

    splash screen

--- app/src/main/java/com/anand/activity/LoginActivity.java
+++ app/src/main/java/com/anand/activity/LoginActivity.java
@@ -1,13 +1,15 @@
-package com.anand;
+package com.anand.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SplashScreenActivity extends AppCompatActivity {
+import com.anand.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        setContentView(R.layout.activity_login);
     }
 }
